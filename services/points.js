ee3683b2533e20329f585954ee07061b6c63a5eb
sambell <bell.sam@Outlook.com>
2019-10-19 10:42:58 -0500

    logic for fetching user transfer history

--- services/points.js
+++ services/points.js
@@ -16,12 +16,13 @@ let points = {
     },
     getUserTransferHistory: async function(user_id, from_date, to_date) {
         let transfers = await db.any(`select 
-                        u.first_name,
-                        u.last_name,
+                        to_json(u.*) as to_user,
+                        CASE WHEN t.from_user = $1 THEN 'giving' ELSE 'receiving' END AS direction,
                         t.amount,
                         t.date_created
                         from transfers t
                         left join users u on u.user_id = t.to_user
+                        left join users u2 on u2.user_id = t.from_user
                         where (t.from_user = $1 or t.to_user = $1) 
                         and (t.date_created::DATE >= $2 and t.date_created::DATE <= $3)
                         order by t.date_created desc;`, [user_id, from_date, to_date])
