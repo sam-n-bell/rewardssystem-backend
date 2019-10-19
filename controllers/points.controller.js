40b8122bbdb52a32088553132cd41d4ba82c62c0
sambell <bell.sam@Outlook.com>
2019-10-19 10:39:38 -0500

    logic for fetching user transfer history

--- controllers/points.controller.js
+++ controllers/points.controller.js
@@ -31,10 +31,10 @@ let PointsController = {
         try {
             let from_date = req.query.fromDate;
             let to_date = req.query.toDate;
-            if (!_.isNil((from_date)) ||  from_date === "") {
+            if (_.isNil((from_date)) ||  from_date === "") {
                 from_date = moment().format('YYYY-MM-DD')
             }
-            if (!_.isNil((to_date)) ||  to_date === "") {
+            if (_.isNil((to_date)) ||  to_date === "") {
                 to_date = moment().add(30, 'days').format('YYYY-MM-DD')
             }
             let transfers = await services.points.getUserTransferHistory(req.locals.user_id, from_date, to_date)
